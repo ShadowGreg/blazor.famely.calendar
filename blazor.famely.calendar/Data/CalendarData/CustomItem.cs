acd8b9e9c9f75ce3c534bb4784f80294533f71df
Greg Osetrov <shadow-greg@yandex.ru>
2024-03-15 08:03:28 +0300

    add files

---
+++ blazor.famely.calendar/Data/CalendarData/CustomItem.cs
@@ -0,0 +1,12 @@
+﻿using Heron.MudCalendar;
+using MudBlazor;
+
+namespace blazor.famely.calendar.Data.CalendarData
+{
+    public class CustomItem : CalendarItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public Color Color { get; set; } = Color.Primary;
+    }
+}
